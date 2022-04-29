26aaf159211fd6038ed93d04dea43c1a08c81e28
Mario Forcelli <mario-forcelli@hotmail.com>
2022-04-28 21:06:27 -0300

    Adicionada responsividade para o componente ContentTop

--- src/components/ContentTop/styles.js
+++ src/components/ContentTop/styles.js
@@ -1,12 +1,31 @@
 import styled from 'styled-components'
 import { Colors as c } from '../../styles/constants'
 import background from '../../assets/bg-intro-desktop.svg'
+import backgroundMobile from '../../assets/bg-intro-mobile.svg' 
+import mockups from '../../assets/mockups.png' 
 
 export const Container = styled.section`
     height: 700px;
     width: 100%;
     background: url(${background}) left 700px top -250px no-repeat;
     background-size: cover ;
+
+    @media(max-width: 1360px) {
+        background: url(${background}) left 400px top -150px no-repeat;
+        height: 600px;
+        
+    }
+    @media(max-width: 960px) {
+        
+        height: 400px;
+        
+    }
+
+    @media(max-width: 860px) {
+        background: none;
+        height: 1000px;
+    }   
+
     /* background-size: cover 120%;
     background-position: -50px -150px;
     background-repeat: no-repeat; */
@@ -17,11 +36,23 @@ export const Wrapper = styled.div`
     margin: auto;
     max-width: calc(1300px + 1.5em);
     display: block;
+
+    @media(max-width: 860px) {
+        height: 100%;
+        
+    }
 `
 
 export const Content = styled.div`
     display: flex;
     justify-content: space-between;
+
+    @media(max-width: 860px) {
+        flex-direction: column-reverse;
+        align-items: center;
+        justify-content: center;
+        height: 100%;
+    }   
 `
 
 export const SectionLeft = styled.div`
@@ -40,6 +71,11 @@ export const SectionTitle = styled.h1`
     font-size: 50px;
     color: ${c.primary.darkBlue};
     font-weight: 300;
+
+    @media (max-width: 860px) {
+        text-align: center;
+        
+    }
 `
 
 export const SectionDescription = styled.span`
@@ -47,15 +83,41 @@ export const SectionDescription = styled.span`
     width: 500px;
     font-size: 18px;
 
+    @media (max-width: 860px) {
+        text-align: center;
+        
+    }
+
 `
 
 export const SectionRight = styled.div`
     display: flex;
     flex: 1;
+    width: 100%;
+
+    @media(max-width: 860px) {
+        flex: 2;
+        background: url(${mockups}) center  top 0 no-repeat, url(${backgroundMobile}) center no-repeat;
+        background-size: 500px, cover;
+    }
+
 
     & img{
-    
         margin-left: 300px;
+
+        @media(max-width: 1360px) {
+        margin-left: 0px;
+        width: 100%;
+        
+        }
+
+        @media(max-width: 860px) {
+            margin-left: 0px;
+            width: 100%;
+            display: none ;
+        }
+
+
     }
 
 
@@ -65,6 +127,10 @@ export const ButtonContainer = styled.div`
     width: 500px;
     margin-top: 30px;
 
+    @media (max-width: 860px) {
+        justify-content: center;
+        
+    }
 `
 
 export const InviteButton = styled.button`
